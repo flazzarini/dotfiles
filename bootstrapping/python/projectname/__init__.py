dd7059f4ffe63d075543fe097f3e67e32ee0745f
Frank Lazzarini <frank.lazzarini@dt.ept.lu>
2013-11-06 13:48:44 +0100

    Renamed __version into __version__

--- bootstrapping/python/projectname/__init__.py
+++ bootstrapping/python/projectname/__init__.py
@@ -1,5 +1,5 @@
 import logging
 import pkg_resources
 
-__version = pkg_resources.resource_string(__name__, "version.txt").strip()
+__version__ = pkg_resources.resource_string(__name__, "version.txt").strip()
 LOG = logging.getLogger(__name__)
