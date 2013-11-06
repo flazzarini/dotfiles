2a446bac5baaab2b0832c3da4005e9f0418ddda8
Frank Lazzarini <frank.lazzarini@dt.ept.lu>
2013-11-06 13:43:06 +0100

    Cleanup on setup.py

--- bootstrapping/python/setup.py
+++ bootstrapping/python/setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup, find_packages
-from projectname import __version
+from os.path import join
 
 NAME = "projectname"
 DESCRIPTION = "A project description."
 AUTHOR = "Frank Lazzarini"
 AUTHOR_EMAIL = "flazzarini@gmail.com"
-VERSION = __version
+VERSION = open(join(NAME, 'version.txt')).read().strip()
+LONG_DESCRIPTION = open("README.rst").read()
 
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=open("README.rst").read(),
+    long_description=LONG_DESCRIPTION,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="Private",
     include_package_data=True,
     install_requires=[
-      ],
+    ],
+    entry_points={
+        'console_sciprts': []
+    },
+    dependency_links=[],
     packages=find_packages(exclude=["tests.*", "tests"]),
+    zip_safe=False,
 )
