854261422ef6bb63d3c287be3ac4c852763b0246
Frank Lazzarini <flazzarini@gmail.com>
2014-02-09 17:52:25 +0100

    Should sleep for 45minutes

--- i3/scripts/wallpapers.sh
+++ i3/scripts/wallpapers.sh
@@ -3,5 +3,5 @@
 while true; do
     find ~/pictures/wallpapers/ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
         shuf -n1 -z | xargs -0 feh --bg-fill --auto-zoom
-    sleep 5
+    sleep 45m
 done
