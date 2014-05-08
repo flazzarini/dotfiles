bfed30c334b996f7589729302c41de87895a1b17
Frank Lazzarini <flazzarini@gmail.com>
2014-05-08 21:46:27 +0200

    Catch exceptions

--- i3/scripts/jukebox_playing.py
+++ i3/scripts/jukebox_playing.py
@@ -1,13 +1,16 @@
 import urllib.request
 import json
 
-url = "http://mixer.gefoo.org/playing"
-req = urllib.request.Request(url, None)
+try:
+    url = "http://mixer.gefoo.org/playing"
+    req = urllib.request.Request(url, None)
 
-# Read response
-resp = urllib.request.urlopen(req).readall().decode('utf-8')
+    # Read response
+    resp = urllib.request.urlopen(req).readall().decode('utf-8')
 
-if json.loads(resp):
-    res = json.loads(resp)
-    print("{0} - {1}".format(res['artist'],
-                             res['title']))
+    if json.loads(resp):
+        res = json.loads(resp)
+        print("{0} - {1}".format(res['artist'],
+                                 res['title']))
+except urllib.URLError:
+    print("No Connection")
