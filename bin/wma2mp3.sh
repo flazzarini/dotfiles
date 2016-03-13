d3f4ea6a0a219dd33c28f26ba7bc449ef804f50f
Frank Lazzarini <flazzarini@gmail.com>
2016-03-13 18:05:02 +0100

    Some bug fixes on script

--- bin/wma2mp3.sh
+++ bin/wma2mp3.sh
@@ -2,12 +2,12 @@
 
 #convert wma to mp3
 
-FILES="*.m4a"
+FILES="*.wma"
 
 for F in $FILES
 
 do
-    newname=`basename "$F" .m4a`
+    newname=`basename "$F" .wma`
     echo $newname
     ffmpeg -i "$F" -acodec libmp3lame -ac 2 -ab 192k -ar 44100 "$newname.mp3"
 done
