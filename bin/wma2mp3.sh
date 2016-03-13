30ecf6e9756212193d2b56759e3f2f7605fb9d97
Frank Lazzarini <flazzarini@gmail.com>
2016-03-13 17:59:14 +0100

    Added wma2mp3 converter

---
+++ bin/wma2mp3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#convert wma to mp3
+
+FILES="*.m4a"
+
+for F in $FILES
+
+do
+    newname=`basename "$F" .m4a`
+    echo $newname
+    ffmpeg -i "$F" -acodec libmp3lame -ac 2 -ab 192k -ar 44100 "$newname.mp3"
+done
