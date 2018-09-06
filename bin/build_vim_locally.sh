f612858fa65534f8ef9ef703d2f84b21d078eba9
Frank Lazzarini <frank.lazzarini@post.lu>
2018-09-06 13:28:47 +0200

    Added pull and changed dest folder

--- bin/build_vim_locally.sh
+++ bin/build_vim_locally.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 BASE_COMPILE_PATH=`realpath ~/downloads/tmp_build_vim`
-PREFIX_PATH=`realpath ~/opt/vim_build/`
+PREFIX_PATH=`realpath ~/opt/vim/`
 
 # Change directory to ~/downloads
 mkdir -p $BASE_COMPILE_PATH && cd $BASE_COMPILE_PATH
 
 [ -f $BASE_COMPILE_PATH/vim ] || cd $BASE_COMPILE_PATH && git clone --depth 1 https://github.com/vim/vim.git
-cd $BASE_COMPILE_PATH/vim && git checkout master
+cd $BASE_COMPILE_PATH/vim && git checkout master && git pull
 
 cd $BASE_COMPILE_PATH/vim && ./configure \
     --enable-python3interp=yes \
