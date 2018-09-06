149338d9ae31d7ad42450f57c792d19f064ab7d9
Frank Lazzarini <frank.lazzarini@post.lu>
2018-09-06 13:26:13 +0200

    Script to compile vim locally

---
+++ bin/build_vim_locally.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASE_COMPILE_PATH=`realpath ~/downloads/tmp_build_vim`
+PREFIX_PATH=`realpath ~/opt/vim_build/`
+
+# Change directory to ~/downloads
+mkdir -p $BASE_COMPILE_PATH && cd $BASE_COMPILE_PATH
+
+[ -f $BASE_COMPILE_PATH/vim ] || cd $BASE_COMPILE_PATH && git clone --depth 1 https://github.com/vim/vim.git
+cd $BASE_COMPILE_PATH/vim && git checkout master
+
+cd $BASE_COMPILE_PATH/vim && ./configure \
+    --enable-python3interp=yes \
+    --enable-luainterp=yes \
+    --with-features=huge \
+    --prefix=$PREFIX_PATH
+
+cd $BASE_COMPILE_PATH/vim && make && make install
