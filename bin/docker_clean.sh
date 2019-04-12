bf5e6425a782d303a47ed87894f7305621c3dcef
Frank Lazzarini <frank.lazzarini@post.lu>
2019-04-12 09:10:08 +0200

    Better cleaning

--- bin/docker_clean.sh
+++ bin/docker_clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker ps --filter "status=exited" | grep 'months ago' | xargs --no-run-if-empty docker rm
+docker ps --filter "status=exited" | grep 'months ago' | awk '{ print $1 }' |xargs --no-run-if-empty docker rm
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 docker volume rm $(docker volume ls -qf dangling=true)
