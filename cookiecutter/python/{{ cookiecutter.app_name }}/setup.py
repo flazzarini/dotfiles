33ea6bf964cc22d82f4dd854d7c85e38a100cd27
Frank Lazzarini <flazzarini@gmail.com>
2020-05-09 08:59:46 +0200

    Use double quotes

--- cookiecutter/python/{{ cookiecutter.app_name }}/setup.py
+++ cookiecutter/python/{{ cookiecutter.app_name }}/setup.py
@@ -5,7 +5,7 @@ NAME = "{{ cookiecutter.app_name }}"
 DESCRIPTION = "{{ cookiecutter.app_description }}"
 AUTHOR = "Frank Lazzarini"
 AUTHOR_EMAIL = "flazzarini@gmail.com"
-VERSION = open(join(NAME, 'version.txt')).read().strip()
+VERSION = open(join(NAME, "version.txt")).read().strip()
 LONG_DESCRIPTION = open("README.rst").read()
 
 setup(
@@ -18,30 +18,30 @@ setup(
     license="Private",
     include_package_data=True,
     install_requires=[
-        'requests >=2.18.4, <3.0',
+        "requests >=2.18.4, <3.0",
     ],
     entry_points={
-        'console_scripts': []
+        "console_scripts": []
     },
     extras_require={
-        'dev': [
-            'sphinx',
-            'sphinx-rtd-theme',
+        "dev": [
+            "sphinx",
+            "sphinx-rtd-theme",
         ],
-        'test': [
-            'pylint',
-            'pyroma',
-            'pytest',
-            'pytest-cov',
-            'pytest-xdist',
-            'radon',
+        "test": [
+            "pylint",
+            "pyroma",
+            "pytest",
+            "pytest-cov",
+            "pytest-xdist",
+            "radon",
         ]
     },
     dependency_links=[],
     packages=find_packages(exclude=["tests.*", "tests"]),
     zip_safe=False,
     classifiers=[
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
     ],
 )
