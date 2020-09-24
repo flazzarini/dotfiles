30ab7878ad38b5b00f4c46dce1ce94313dd83038
Frank Lazzarini <frank.lazzarini@post.lu>
2020-09-24 08:19:35 +0200

    New version of vim 8.2

--- installers/vim/install.sh
+++ installers/vim/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="8.2.0993"
+VERSION="8.2.1736"
 DOWNURL="https://github.com/vim/vim/archive/v$VERSION.tar.gz"
 
 BUILD="vim-$VERSION"
@@ -13,7 +13,10 @@ cd $BUILD && ./configure \
   --prefix="$HOME/opt/vim" \
   --disable-darwin \
   --disable-smack \
+  --enable-multibyte \
   --enable-python3interp \
+  --enable-cscope \
+  --with-features=huge \
   --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu
 make && make install
 rm -f $DOWNLOAD
